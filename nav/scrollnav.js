3dca4325bd0ba6d859dfb36ff2b60e8ccf581c61
Wang Guosheng <201511140243@mail.bnu.edu.cn>
2019-09-27 21:20:16 +0800

    added nav js

---
+++ nav/scrollnav.js
@@ -0,0 +1,39 @@
+
+   $(function () {
+
+    // show hide subnav depending on scroll direction
+    var position = $(window).scrollTop();
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll > position) {
+
+            //only piece that matters
+            $('nav')
+            	.stop(true, false)
+                .removeClass('up')
+                .addClass('down');
+
+
+            // scrolling downwards, only here for dev purposes
+            console.log('moving DOWN the page');
+            $('input').val('down');
+
+        } else {
+            //only piece that matters
+            $('nav')
+                .stop(true, false)
+                .removeClass('down')
+                .addClass('up');
+
+
+            // scrolling upwards 
+            console.log('moving UP the page');
+            $('input').val('up');
+        }
+
+        position = scroll;
+    });
+
+});
