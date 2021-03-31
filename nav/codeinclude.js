474ff3e6b72741f0a8f0f3ecb2599a55e991d04b
Wang Guosheng <wguosh@gmail.com>
2021-04-01 00:03:28 +0800

    edit codeinclude.js

--- nav/codeinclude.js
+++ nav/codeinclude.js
@@ -1,7 +1,12 @@
-$(function() {
-  var url = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/day1/hello_world.rb";
-  $.get(url, function(data,status){
-    console.log(data)
-    $("#includedContent_2021_03_31_22_19").append(data.replace('\n','<p>'));
-  });
-});
+// $(function() {
+//   var url = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/day1/hello_world.rb";
+//   $.get(url, function(data,status){
+//     console.log(data)
+//     $("#includedContent_2021_03_31_22_19").append(data.replace('\n','<p>'));
+//   });
+// });
+
+$(function(){
+    var url = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/day1/hello_world.html";
+    $("includedContent_2021_03_31_22_19").load(url_2021_03_31_22_19)
+})
