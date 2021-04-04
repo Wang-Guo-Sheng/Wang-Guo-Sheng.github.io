9fff2083d1a2954d403e678db4f98b42ac755013
Wang Guosheng <wguosh@gmail.com>
2021-04-04 20:17:36 +0800

    edit post

--- _posts/tech/2021-04-03-Getting-started-with-Ruby-3.md
+++ _posts/tech/2021-04-03-Getting-started-with-Ruby-3.md
@@ -12,11 +12,22 @@ tags:
 
 # 元编程
 
+- 用Ruby自定义语法
+- 动态地改变类
+
+- 既便于解释器，又便于人类阅读理解。
+
+[Builder](https://github.com/jimweirich/builder)
+
+[ActiveRecord](https://github.com/rails/rails/tree/main/activerecord)
+
 ## 开放类
 
 在某个类名上首次调用`class`会定义一个类；但如果该类已经定义过，再调用`class`会修改先前的类的定义，比如添加方法。因为有随时重定义任何类或对象的自由，所以称为开放类。这种写法让代码实现简单易懂。
 
 `blank.rb`
+
+<div id = "includedContent_2021_04_04_20_09_6905d014"></div>
 
 ```bash
 % ruby blank.rb
@@ -26,6 +37,8 @@ person
 这样，不论是`nil`还是`""`，因为两个类都具有`blank?`方法，就可以用相同的方式判断是否空字符串（鸭子类型）。
 
 `units.rb`把各种单位转成英寸：
+
+<div id = "includedContent_2021_04_04_20_09_82d765c3"></div>
 
 ```bash
 % ruby units.rb
@@ -41,6 +54,8 @@ person
 
 `roman.rb`
 
+<div id = "includedContent_2021_04_04_20_09_95fee552"></div>
+
 ```bash
 10
 90
@@ -50,12 +65,36 @@ NoMethodError
 
 ## 模块
 
+比如要读取一个 `csv` 表格：
 
+### 直接用类实现
 
+`acts_as_csv_class.rb`
 
+<div id = "includedContent_2021_04_04_20_09_9257f3bf"></div>
 
+### 用**宏**实现
 
+通过宏中的方法调用`define_method`添加这组方法：
 
+`acts_as_csv.rb`
+
+<div id = "includedContent_2021_04_04_20_09_819babba"></div>
+
+### 用模块实现：
+
+`acts_as_csv_module.rb`
+
+<div id = "includedContent_2021_04_04_20_09_98f422ce"></div>
+
+- 只要某个模块被另一模块包含，Ruby就会调用该模块的`included`方法。
+
+> 类是可以被实例化的模块。
+
+- 调用该模块的类，在模块中记为`base`。
+
+- 通过`extend`添加**类方法**
+- 通过`include`添加**实例方法**
 
 
 
--- nav/codeinclude.js
+++ nav/codeinclude.js
@@ -46,3 +46,27 @@ $(function(){
     var url_2021_04_02_22_07_1f30db9d = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/day2/tree_init.html";
     $("#includedContent_2021_04_02_22_07_1f30db9d").load(url_2021_04_02_22_07_1f30db9d)
 })
+$(function(){
+    var url_2021_04_04_20_09_6905d014 = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/day3/blank.html";
+    $("#includedContent_2021_04_04_20_09_6905d014").load(url_2021_04_04_20_09_6905d014)
+})
+$(function(){
+    var url_2021_04_04_20_09_82d765c3 = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/day3/units.html";
+    $("#includedContent_2021_04_04_20_09_82d765c3").load(url_2021_04_04_20_09_82d765c3)
+})
+$(function(){
+    var url_2021_04_04_20_09_819babba = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/day3/acts_as_csv.html";
+    $("#includedContent_2021_04_04_20_09_819babba").load(url_2021_04_04_20_09_819babba)
+})
+$(function(){
+    var url_2021_04_04_20_09_9257f3bf = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/day3/acts_as_csv_class.html";
+    $("#includedContent_2021_04_04_20_09_9257f3bf").load(url_2021_04_04_20_09_9257f3bf)
+})
+$(function(){
+    var url_2021_04_04_20_09_98f422ce = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/day3/acts_as_csv_module.html";
+    $("#includedContent_2021_04_04_20_09_98f422ce").load(url_2021_04_04_20_09_98f422ce)
+})
+$(function(){
+    var url_2021_04_04_20_09_95fee552 = "https://raw.githubusercontent.com/Wang-Guo-Sheng/ruby_crash_week/main/day3/roman.html";
+    $("#includedContent_2021_04_04_20_09_95fee552").load(url_2021_04_04_20_09_95fee552)
+})
