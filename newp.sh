e8604bafd80b12b1f9c7f1371b9ceb4192881605
Wang Guosheng <wguosh@gmail.com>
2021-11-12 20:09:06 +0800

    edit script

--- newp.sh
+++ newp.sh
@@ -3,6 +3,11 @@
 # Example:
 # newp book "The Time of the Heroes" "https://asiasuptour.org/wp-content/uploads/2019/10/placeholder.png" llosa barsas
 
+currdir=`pwd`
+rootdir=${0%/*}
+cd ${rootdir}
+git pull -ff-only
+
 title=$2
 imgurl=$3
 tags=${@:4:99}
@@ -41,7 +46,7 @@ datetime=`date +%Y-%m-%d\ %H:%M`
 filedate=`date -d "${datetime}" +%Y-%m-%d`
 filetitle="${title// /-}"
 fbasename="${filedate}-${filetitle}"
-postpath="./_posts/${page}/${fbasename}.md"
+postpath="${0%/*}/_posts/${page}/${fbasename}.md"
 
 yaml=("---"
     "layout: blog"
@@ -63,4 +68,6 @@ for y in "${yaml[@]}"; do
     echo ${y} | tee -a ${postpath}
 done
 
+smerge ${rootdir}
 xdg-open ${postpath}
+cd ${currdir}
